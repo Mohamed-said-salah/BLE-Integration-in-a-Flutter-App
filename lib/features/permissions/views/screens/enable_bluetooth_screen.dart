1b50b3f44d0b70e04643606023a6df61e6621e06
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 09:37:55 +0200

    Created: Enable Bluetooth Screen

---
+++ lib/features/permissions/views/screens/enable_bluetooth_screen.dart
@@ -0,0 +1,116 @@
+import 'package:ble_integration_in_flutter_app/features/permissions/view_models/bluetooth/bluetooth_status_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../../constants/fonts/font_families_names.dart';
+import '../../../../constants/animations/animation_assets_paths.dart';
+import '../widgets/buttons/back_to_find_devices_screen_button.dart';
+import '../widgets/buttons/enable_bluetooth_button.dart';
+
+class EnableBluetoothScreen extends StatelessWidget {
+  static const String routeName = '/enable_bluetooth_screen';
+
+  const EnableBluetoothScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocBuilder<BluetoothStatusCubit, BluetoothStatusState>(
+        builder: (context, state) {
+          if (state is BluetoothEnabled) {
+            return const BluetoothEnabledView();
+          }
+
+          return const EnableBlueToothView();
+        },
+      ),
+    );
+  }
+}
+
+class EnableBlueToothView extends StatefulWidget {
+  const EnableBlueToothView({super.key});
+
+  @override
+  State<EnableBlueToothView> createState() => _EnableBlueToothViewState();
+}
+
+class _EnableBlueToothViewState extends State<EnableBlueToothView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Spacer(flex: 2),
+        SizedBox(
+          height: 250,
+          width: 250,
+          child: Lottie.asset(AnimationAssetsPaths.enableBluetooth),
+        ),
+        const Spacer(flex: 1),
+        const Text(
+          'Enable Bluetooth',
+          style: TextStyle(
+            fontSize: 24,
+            fontFamily: FontFamiliesNames.hostGroteskSemiBold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'We need to enable Bluetooth and allow all of it\'s permissions to ensure this app works properly',
+          style: TextStyle(
+            fontSize: 16,
+            fontFamily: FontFamiliesNames.hostGroteskRegular,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const Spacer(flex: 1),
+        const EnableBluetoothButton(),
+        const Spacer(flex: 1),
+      ],
+    );
+  }
+}
+
+class BluetoothEnabledView extends StatelessWidget {
+  const BluetoothEnabledView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Spacer(flex: 2),
+        SizedBox(
+          height: 200,
+          width: 200,
+          child: Lottie.asset(
+            AnimationAssetsPaths.doneBlueAnimations,
+          ),
+        ),
+        const Spacer(flex: 1),
+        const Text(
+          'Bluetooth Enabled',
+          style: TextStyle(
+            fontSize: 24,
+            fontFamily: FontFamiliesNames.hostGroteskSemiBold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'You can now start scanning and connect for bluetooth devices',
+          style: TextStyle(
+            fontSize: 16,
+            fontFamily: FontFamiliesNames.hostGroteskRegular,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const Spacer(flex: 1),
+        const BackToFindDevicesScreenButton(),
+        const Spacer(flex: 1),
+      ],
+    );
+  }
+}
