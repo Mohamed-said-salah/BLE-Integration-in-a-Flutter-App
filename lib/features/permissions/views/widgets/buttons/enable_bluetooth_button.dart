6df20d5dfaf40021775e18c6315a38111ed512fb
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 09:35:11 +0200

    Created: Enable Bluetooth Button

---
+++ lib/features/permissions/views/widgets/buttons/enable_bluetooth_button.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import '../../../../../constants/fonts/font_families_names.dart';
+
+class EnableBluetoothButton extends StatelessWidget {
+  const EnableBluetoothButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+        child: InkWell(
+          onTap: () {
+            FlutterBluePlus.turnOn(timeout: 30);
+          },
+          borderRadius: BorderRadius.circular(12),
+          child: Ink(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 78, 177, 223),
+                  Color.fromARGB(255, 102, 158, 221),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Enable Bluetooth',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontFamily: FontFamiliesNames.hostGroteskRegular,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Icon(
+                    Icons.bluetooth_connected_rounded,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
