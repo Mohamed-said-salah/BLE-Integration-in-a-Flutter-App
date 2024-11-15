7891b7f5dde186aab7f4f40d389cb6095de89ce0
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 19:08:15 +0200

    created: find near devices singleton cubit

--- lib/features/nearby_devices/view_models/cubit/find_near_devices_cubit.dart
+++ lib/features/nearby_devices/view_models/cubit/find_near_devices_cubit.dart
@@ -6,7 +6,7 @@ part 'find_near_devices_state.dart';
 
 class FindNearDevicesCubit extends Cubit<FindNearDevicesState> {
   // Private constructor for singleton pattern
-  FindNearDevicesCubit._internal() : super(FindNearDevicesInitial());
+  FindNearDevicesCubit._internal() : super(const FindNearDevicesInitial([]));
 
   // Singleton instance
   static final FindNearDevicesCubit _instance =
@@ -19,13 +19,13 @@ class FindNearDevicesCubit extends Cubit<FindNearDevicesState> {
 
   void startScan() async {
     if (state is! BlueToothScanning) {
-      emit(BlueToothScanning());
+      emit(BlueToothScanning(_devices));
 
       _devices.clear();
 
       if (FlutterBluePlus.adapterStateNow == BluetoothAdapterState.on) {
         // Start scanning
-        await FlutterBluePlus.startScan(timeout: const Duration(seconds: 15));
+        await FlutterBluePlus.startScan(timeout: const Duration(seconds: 10));
 
         // Listen for scan results
         FlutterBluePlus.onScanResults.listen((results) {
@@ -33,18 +33,21 @@ class FindNearDevicesCubit extends Cubit<FindNearDevicesState> {
             if (!_devices.contains(r.device)) {
               _devices.add(r.device);
             }
+
+            // Emit the devices found state with the updated list of devices
+            emit(BlueToothScanning(List.from(_devices)));
           }
         });
 
         // stop the scanning after 15 seconds timeout and if result is empty
         // it will emit the BlueToothScanStopped state
-        Future.delayed(const Duration(seconds: 15)).then((_) async {
+        Future.delayed(const Duration(seconds: 10)).then((_) async {
           if (FlutterBluePlus.isScanningNow) {
             await FlutterBluePlus.stopScan();
           }
 
           if (_devices.isEmpty) {
-            emit(BlueToothScanStopped());
+            emit(BlueToothScanStopped(_devices));
           } else {
             // Emit the devices found state with the updated list of devices
             emit(BlueToothFoundDevices(List.from(_devices)));
@@ -52,7 +55,7 @@ class FindNearDevicesCubit extends Cubit<FindNearDevicesState> {
         });
       } else {
         // when the bluetooth is not enabled
-        emit(BlueToothScanStopped());
+        emit(BlueToothScanStopped(_devices));
       }
     }
   }
@@ -60,6 +63,6 @@ class FindNearDevicesCubit extends Cubit<FindNearDevicesState> {
   void stopScan() async {
     await FlutterBluePlus.stopScan();
 
-    emit(BlueToothScanStopped());
+    emit(BlueToothScanStopped(_devices));
   }
 }
--- lib/features/nearby_devices/view_models/cubit/find_near_devices_state.dart
+++ lib/features/nearby_devices/view_models/cubit/find_near_devices_state.dart
@@ -1,23 +1,38 @@
 part of 'find_near_devices_cubit.dart';
 
 sealed class FindNearDevicesState extends Equatable {
-  const FindNearDevicesState();
+  const FindNearDevicesState(this.devices);
 
-  @override
-  List<Object> get props => [];
-}
-
-final class FindNearDevicesInitial extends FindNearDevicesState {}
-
-final class BlueToothScanning extends FindNearDevicesState {}
-
-final class BlueToothScanStopped extends FindNearDevicesState {}
-
-final class BlueToothFoundDevices extends FindNearDevicesState {
   final List<BluetoothDevice> devices;
-
-  const BlueToothFoundDevices(this.devices);
 
   @override
   List<Object> get props => [devices];
+}
+
+final class FindNearDevicesInitial extends FindNearDevicesState {
+  const FindNearDevicesInitial(super.devices);
+
+  @override
+  List<Object> get props => [super.devices];
+}
+
+final class BlueToothScanning extends FindNearDevicesState {
+  const BlueToothScanning(super.devices);
+
+  @override
+  List<Object> get props => [super.devices];
+}
+
+final class BlueToothScanStopped extends FindNearDevicesState {
+  const BlueToothScanStopped(super.devices);
+
+  @override
+  List<Object> get props => [super.devices];
+}
+
+final class BlueToothFoundDevices extends FindNearDevicesState {
+  const BlueToothFoundDevices(super.devices);
+
+  @override
+  List<Object> get props => [super.devices];
 }
