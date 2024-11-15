e080a674286d8f8cb622b43af773162a855e9a13
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 09:40:34 +0200

    created: Back to find near by devices button

---
+++ lib/features/permissions/views/widgets/buttons/back_to_find_devices_screen_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../constants/fonts/font_families_names.dart';
+
+class BackToFindDevicesScreenButton extends StatelessWidget {
+  const BackToFindDevicesScreenButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          borderRadius: BorderRadius.circular(12),
+          child: Ink(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Return to find nearby devices',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: FontFamiliesNames.hostGroteskRegular,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Icon(
+                    Icons.bluetooth_searching_sharp,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
