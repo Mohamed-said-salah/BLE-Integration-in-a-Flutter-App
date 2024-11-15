4db3709842d1e1752908a110614964c949e7be14
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 09:37:16 +0200

    Defined: Lottie Animations Paths

---
+++ lib/constants/animations/animation_assets_paths.dart
@@ -0,0 +1,7 @@
+abstract class AnimationAssetsPaths {
+  static const String enableBluetooth = 'assets/lottie/enable_bluetooth.json';
+  static const String enableLocation = 'assets/lottie/enable_location.json';
+  static const String bluetoothSearch = 'assets/lottie/bluetooth_search.json';
+  static const String doneBlueAnimations =
+      'assets/lottie/done_blue_animations.json';
+}
