554564e14a708bf24b1f0e73466658afa69485df
Mohamed Saied <71166528+Mohamed-said-salah@users.noreply.github.com>
2024-11-15 09:37:33 +0200

    Defined: Font FAmiles names

---
+++ lib/constants/fonts/font_families_names.dart
@@ -0,0 +1,5 @@
+abstract class FontFamiliesNames {
+  static const String eduAUVICWANTPreSemiBold = 'EduAUVICWANTPre-SemiBold';
+  static const String hostGroteskSemiBold = 'HostGrotesk-SemiBold';
+  static const String hostGroteskRegular = 'HostGrotesk-Regular';
+}
